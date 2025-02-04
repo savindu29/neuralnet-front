74ae16d60bec955a3977483f36beba0444e91650
Savindu Panagoda <86566770+savindu29@users.noreply.github.com>
2025-02-04 21:29:31 +0530

    fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen bg-gray-900  text-white p-4">
     <Landing/>
-    <div className="flex w-full h-[45vh] py-4 gap-4 ">
-     < div className="flex h-[45vh]  flex-col w-1/3 gap-4">
+    <div className="flex lg:flex-row flex-col w-full h-[45vh] py-4 gap-4 ">
+     < div className="flex h-[45vh]  flex-col lg:w-1/3 w-full  gap-4">
      <Left/>
       </div>
-      < div className="flex h-[45vh]  flex-col w-2/3 gap-4">
+      < div className="flex h-[45vh]  flex-col lg:w-2/3 w-full gap-4">
      <Right/>
       </div>
   
