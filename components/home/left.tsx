fa1f97b810cf6c3e7f2ac6348ce905ac8956db11
Savindu Panagoda <86566770+savindu29@users.noreply.github.com>
2025-02-04 22:03:10 +0530

    edit classification ui

--- components/home/left.tsx
+++ components/home/left.tsx
@@ -7,14 +7,16 @@ export default function Left() {
   return (
     <div className="flex flex-col w-full p-6 rounded-xl bg-white shadow-lg text-black h-full">
       {/* Classify EEG Data section */}
+      <Link href="/classify">
       <div className="flex justify-between items-center mb-3 border-b">
         <div className="text-black text-2xl font-semibold mb-2">Classify EEG Data</div>
-        <Link href="/classify">
+        
           <Button variant="ghost" className="flex items-center w-32 text-8xl hover:bg-white">
             <MoveRight />
           </Button>
-        </Link>
+       
       </div>
+      </Link>
 
       {/* XAI section
       <div className="flex justify-between items-center mb-3 border-b">
